2c499b6260eb56ea9b8b467652bd300e23c9e538
15153246027 <652992429@qq.com>
2020-05-15 12:45:50 +0800

    sss

--- plugins/axios.js
+++ plugins/axios.js
@@ -1,6 +1,6 @@
 
 // axios扩展配置
 const axiosConfig = function ({ $axios, redirect }) {
-  $axios.setBaseURL('http://121.36.173.121/')
+  // $axios.setBaseURL('http://121.36.173.121/')
 }
 module.exports = axiosConfig
